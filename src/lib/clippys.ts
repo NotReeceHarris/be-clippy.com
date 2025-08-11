b38a2f4e8b9bac0bf3ec141ab93064caf11d6134
Reece Harris <reece.harris98@protonmail.com>
2025-08-11 22:00:08 +0100

    feat: add new Clippy characters and associated images

--- src/lib/clippys.ts
+++ src/lib/clippys.ts
@@ -2,12 +2,22 @@ const clippys = [
     {
         "name": "OG Clippy",
         "image": "clippy.png"
-    },
-    {
+    },{
         "name": "Cowboy",
         "image": "cowboy.png"
-    },
-    {
+    },{
+        "name": "Master Clippy",
+        "image": "master.png"
+    },{
+        "name": "Yummy",
+        "image": "yummy.png"
+    },{
+        "name": "Miku",
+        "image": "miku.png"
+    },{
+        "name": "Safety First",
+        "image": "safety.png"
+    },{
         "name": "Orange Clippy",
         "image": "orange.png"
     }

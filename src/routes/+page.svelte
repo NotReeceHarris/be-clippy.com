c71f24a38401332f9d8827238b28bab2423a66e1
Reece Harris <reece.harris98@protonmail.com>
2025-08-11 22:23:27 +0100

    style: update canvas styling for Clippy characters to improve layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -158,7 +158,7 @@
                         <svg class="z-10 absolute size-8 animate-spin opacity-70" style="color: {getTextColour()};" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
                     {/if}
 
-                    <canvas bind:this={clippyCanvas[clippyIndex]} class="rounded-lg"></canvas>
+                    <canvas bind:this={clippyCanvas[clippyIndex]} class="aspect-square p-8 md:p-0 w-full rounded-lg"></canvas>
                 </div>
             {/each}
 
